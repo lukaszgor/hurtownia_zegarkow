d27b14d34686aa24fa91068ea735b1ddd22f5a12
Łukasz Górski <lukasz.gg13@gmail.com>
2019-04-17 18:47:38 +0200

    first commit

---
+++ hurtowniaZegarkow/hurtowniaZegarkow/Form1.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hurtowniaZegarkow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(@"datasource=127.0.0.1;port=3306;username=root;password=;persistsecurityinfo=True;database=czas");
+
+
+
+            //conn.Open();
+            MySqlCommand cmd = new MySqlCommand("select * from admin where login='"+this.textBox1.Text+"'and pass='"+this.textBox2.Text+"' ; ", conn);
+            MySqlDataReader Reader;
+            try
+            {
+                conn.Open();
+                Reader = cmd.ExecuteReader();
+                int count = 0;
+                while (Reader.Read())
+                {
+                    count = count + 1;
+                }
+                if (count == 1)
+                {
+                    MessageBox.Show("login i haslo poprawne");
+                    this.Hide();
+                    Form2 f2 = new Form2();
+                    f2.ShowDialog();
+                }
+                else if(count > 1)
+                {
+                    MessageBox.Show("zduplikowane");
+                }
+                else
+                {
+                    MessageBox.Show("nie poprawne!");
+                }
+                conn.Close();
+            }
+            catch (Exception ee)
+            {
+                Console.Write(ee);
+            }
+
+        }
+    }
+}
---
+++ hurtowniaZegarkow/hurtowniaZegarkow/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hurtowniaZegarkow
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            zamDoprod zamdoprod = new zamDoprod();
+            zamdoprod.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            obsZamklie obszamklie = new obsZamklie();
+            obszamklie.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ListaKlientow listaklientow = new ListaKlientow();
+            listaklientow.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ListaModeli listamodeli = new ListaModeli();
+            listamodeli.ShowDialog();
+        }
+    }
+}
